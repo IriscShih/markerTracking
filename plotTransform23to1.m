e1d32dc80e3bf46f137b8041bfd16446646d112b
Iris Shih <iris.cshih@gmail.com>
2014-10-28 15:30:38 +0100

    modified tracking plot

--- plotTransform23to1.m
+++ plotTransform23to1.m
@@ -1,8 +1,10 @@
 for m = 1:54
-scatter3([m123_T2(1,m)],[m123_T2(2,m)],[m123_T2(3,m)],'red','o')
+scatter3([m123_T2(1,m)],[m123_T2(2,m)],[m123_T2(3,m)],'blue','o')
+% quiver3([m123_T2(1,m)],[m123_T2(2,m)],[m123_T2(3,m)],[m123_T2(4,m)],[m123_T2(5,m)],[m124_T2(6,m)],0.005, 'red')
 hold on
 scatter3([m123_T3(1,m)],[m123_T3(2,m)],[m123_T3(3,m)],'green','*')
 hold on
-scatter3([m123_1_ume(1,m)],[m123_1_ume(2,m)],[m123_1_ume(3,m)],'blue','+')
+scatter3([m123_1_ume(1,m)],[m123_1_ume(2,m)],[m123_1_ume(3,m)],'red','+')
+% quiver3([m123_1_ume(1,m)],[m123_1_ume(2,m)],[m123_1_ume(3,m)],[m123_1_ume(4,m)],[m123_1_ume(5,m)],[m123_1_ume(6,m)],0.005, 'blue')
 hold on
 end
--- plotTransform24to1.m
+++ plotTransform24to1.m
@@ -1,8 +1,11 @@
 for m = 1:260
-scatter3([m124_T2(1,m)],[m124_T2(2,m)],[m124_T2(3,m)],'red','o')
+scatter3([m124_T2(1,m)],[m124_T2(2,m)],[m124_T2(3,m)],'blue','o')
+% quiver3([m124_T2(1,m)],[m124_T2(2,m)],[m124_T2(3,m)],[m124_T2(4,m)],[m124_T2(5,m)],[m124_T2(6,m)],0.005, 'blue')
 hold on
-scatter3([m124_T4(1,m)],[m124_T4(2,m)],[m124_T4(3,m)],'green','*')
+scatter3([m124_T4(1,m)],[m124_T4(2,m)],[m124_T4(3,m)],'cyan','*')
+% quiver3([m124_T4(1,m)],[m124_T4(2,m)],[m124_T4(3,m)],[m124_T4(4,m)],[m124_T4(5,m)],[m124_T4(6,m)],0.005, 'cyan')
 hold on
-scatter3([m124_1_ume(1,m)],[m124_1_ume(2,m)],[m124_1_ume(3,m)],'blue','+')
+scatter3([m124_1_ume(1,m)],[m124_1_ume(2,m)],[m124_1_ume(3,m)],'red','+')
+% quiver3([m124_1_ume(1,m)],[m124_1_ume(2,m)],[m124_1_ume(3,m)],[m124_1_ume(4,m)],[m124_1_ume(5,m)],[m124_1_ume(6,m)],0.005, 'red')
 hold on
 end
